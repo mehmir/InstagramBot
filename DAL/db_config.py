803d479489bfc90e437ad683228ef69659f56061
Mehdi Mirzaei <mehdi.mirzaei1390@gmail.com>
2021-08-31 12:19:40 +0430

    Update db_config.py

--- DAL/db_config.py
+++ DAL/db_config.py
@@ -1,3 +1,2 @@
 
-# connection_string = 'mssql+pyodbc://sa:654321$pot@192.168.100.2/SOCIAL_NETWORK_BI?driver=SQL+Server'
-connection_string = 'mssql+pyodbc://sonetbot_sa:654321$pot@95.216.56.89,2016/sonetbot_SOCIAL_NETWORK_BI?driver=SQL+Server'
+connection_string = 'mssql+pyodbc://user:pass@server,port/db_name?driver=SQL+Server'
